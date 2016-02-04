bcdd3e11cf7b5d5762639bf943929277afc2a34f
ricardobarros95 <ricardobarros95@gmail.com>
2016-02-04 15:08:03 +0000

    playerMovement/playerCamera

---
+++ UNITY/Assets/Resources/Scripts/Player/PlayerCamera.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCamera : MonoBehaviour {
+
+    public GameObject playerTarget;
+    public float rotateSpeed = 5;
+    Vector3 offset;
+	// Use this for initialization
+	void Start () {
+        offset = playerTarget.transform.position - transform.position;
+        Cursor.lockState = CursorLockMode.Locked;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () 
+    {
+        float horizontal = InputManager.GetAxies("Mouse X") * rotateSpeed;
+        playerTarget.transform.Rotate(0, horizontal, 0);
+
+        float angle = playerTarget.transform.eulerAngles.y;
+        Quaternion rotation = Quaternion.Euler(0, angle, 0);
+        transform.position = playerTarget.transform.position - (rotation * offset);
+         
+        transform.LookAt(playerTarget.transform);
+	}
+}
---
+++ UNITY/Assets/Resources/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float movementSpeed = 10;
+    CharacterController controller;
+    PlayerStats player;
+    Vector3 offset;
+	// Use this for initialization
+	void Start () {
+        controller = GetComponent<CharacterController>();
+        player = GetComponent<PlayerStats>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (Input.GetKey(KeyCode.W))
+            controller.Move(transform.forward * Time.deltaTime * movementSpeed);
+        if (Input.GetKey(KeyCode.D)) 
+            controller.Move(transform.right * movementSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.A))
+            controller.Move(-transform.right * movementSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.S))
+            controller.Move(-transform.forward * Time.deltaTime * movementSpeed);
+	}
+}
