a42b5c2bc1a5e2dbd423f0bcd0282daa759a3582
Zeeshan Abid <zeeshan595@googlemail.com>
2016-02-14 13:33:30 +0000

    Custom Network Manager

---
+++ UNITY/Assets/Resources/Scripts/Manager/CustomNetworkManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class CustomNetworkManager : NetworkManager
+{
+    [SerializeField]
+    private Transform[] spawnPositions;
+
+    public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
+    {
+        var player = (GameObject)GameObject.Instantiate(playerPrefab, spawnPositions[Random.Range(0, spawnPositions.Length)].position, spawnPositions[Random.Range(0, spawnPositions.Length)].rotation);
+        NetworkServer.AddPlayerForConnection(conn, player, playerControllerId);
+    }
+
+    private void Start()
+    {
+        StartupHost();
+    }
+
+    public void StartupHost()
+    {
+        SetPort();
+        NetworkManager.singleton.StartHost();
+    }
+
+    private void SetPort()
+    {
+        NetworkManager.singleton.networkPort = 7777;
+    }
+}
