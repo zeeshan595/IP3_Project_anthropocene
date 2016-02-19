4b323d1eb200e5fe276d39d5c52e212cdded6520
ricardobarros95 <ricardobarros95@gmail.com>
2016-02-19 19:22:06 +0000

    Camera fix

--- UNITY/Assets/Resources/Scripts/Player/PlayerCamera.cs
+++ UNITY/Assets/Resources/Scripts/Player/PlayerCamera.cs
@@ -13,14 +13,16 @@ public class PlayerCamera : MonoBehaviour
     private Transform playerTarget;
     [SerializeField]
     private Vector3 offset;
-
+    private Vector3 originalOffSet;
     private float rightVertical;
+    public bool isWallBanging = false;
 
     // Use this for initialization
     void Start ()
     {
         transform.SetParent(null);
         //Cursor.lockState = CursorLockMode.Locked;
+        originalOffSet = offset;
 	}
 	
 	// Update is called once per frame
@@ -36,6 +38,39 @@ public class PlayerCamera : MonoBehaviour
         rightVertical = Mathf.Clamp(rightVertical, -maxViewRange, maxViewRange);
         Quaternion rotation = Quaternion.Euler(-rightVertical, angle, 0);
         transform.position = playerTarget.transform.position + playerTarget.transform.TransformDirection(offset);
-        transform.LookAt(playerTarget.position + (playerTarget.forward * lookOffset));
+        Vector3 playerPos = playerTarget.position + (playerTarget.forward * lookOffset);
+        transform.LookAt(playerPos);
+        //StartCoroutine(OffSetCamera());
+        Vector3 dir = playerTarget.position - transform.position;
+        if (Physics.Raycast(transform.position, dir, dir.magnitude - 1))
+        {
+            Debug.Log("Object hit!");
+            offset.z = 0;
+        }
+        else
+        {
+            Debug.Log("Why am I being turned off?");
+            offset.z = 10;
+        }
+        //if (isWallBanging)
+        //{
+        //    offset.z = 0;
+        //}
+        //else if(!isWallBanging)
+        //{
+            
+        //    offset.z = originalOffSet.z;
+        //}
 	}
+
+
+    //IEnumerator OffSetCamera()
+    //{
+        
+    //    yield return new WaitForSeconds(1);
+    //    if(!Physics.Raycast(transform.position, dir, dir.magnitude - 1))
+    //    {
+    //        offset.z = originalOffSet.z;
+    //    }
+    //}
 }
