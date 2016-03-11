aadccdfdc7869a795cd6a695f60ac5c730280d83
ricardobarros95 <ricardobarros95@gmail.com>
2016-03-11 12:38:38 +0000

    EndScreen

---
+++ UNITY/Assets/MainMenuPlayButton.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenuPlayButton : MonoBehaviour {
+
+    public void PlayButton()
+    {
+        SceneManager.LoadScene("Lobby");
+    }
+}
